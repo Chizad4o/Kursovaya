1fe8a85f58ca3327771202a2f92eb1004b651ff9
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2022-05-12 22:13:05 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,18 +1,6 @@
 CONSOLETEXT = 'Результат Ваших действий: '
 
-DOCTOR_PROFESSIONS = ["Терапевт", "Окулист", "Ухо горло нос"]
-DIAGNISIS = {
-    DOCTOR_PROFESSIONS[0]: ['Диагноз 1', 'Диагноз 2'],
-    DOCTOR_PROFESSIONS[1]: ['Диагноз 3', 'Диагноз 4'],
-    DOCTOR_PROFESSIONS[2]: ['Диагноз 5', 'Диагноз 6'],
-}
-
 TYPE_OF_FOOD = ["Салат", "Суп", "Горячее", "Напитки"]
-
-DIAGNISISLIST = []
-for dig in DIAGNISIS.values():
-    DIAGNISISLIST.extend(dig)
-
 
 ADMINPASSWORD = '1'
 MAXLENNAME = 20
