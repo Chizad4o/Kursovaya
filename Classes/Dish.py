904bb341c1e306291af14a17d8036ba06ddc1e21
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2022-05-13 23:12:38 +0300

    Update Dish.py

--- Classes/Dish.py
+++ Classes/Dish.py
@@ -1,10 +1,10 @@
-from .Food import Food
+from .Food import Food      #импорт класса Food
 
 
-class Dish(Food):
-    def __init__(self, type_of_food, name, cost, quantity, weight):
-        super().__init__(type_of_food=type_of_food, name=name,  cost = cost, quantity=quantity)
-        self.weight = weight
+class Dish(Food):           #класс, который заимствует данные из Food
+    def __init__(self, type_of_food, name, cost, quantity, weight): #инизиализация экземпляров класса
+        super().__init__(type_of_food=type_of_food, name=name,  cost = cost, quantity=quantity) #доступ к унаследованным методам и передача данных параметров
+        self.weight = weight        #объявление собственной переменной
 
-    def show_info(self):
+    def show_info(self):        #метод вывода информации о блюде, по названию, типу блюда, количеству, весу и цене
         return ''.join([self.name, ' Тип Блюда: ', self.type_of_food, ', Количество: ', str(self.quantity), 'шт. Вес блюда: ', str(self.weight), 'г, Цена: ', str(self.cost), ' ₽'])
