b428f9f3c221fa258b4e2c62fb1f715de6b1fb1a
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2022-05-13 23:12:02 +0300

    Update Food.py

--- Classes/Food.py
+++ Classes/Food.py
@@ -1,22 +1,22 @@
 class Food:
-    def __init__(self, type_of_food, name, cost, quantity):
-        self.type_of_food = type_of_food.lower().title()
-        self.name = name.lower().title()
-        self.cost = cost
-        self.quantity = quantity
+    def __init__(self, type_of_food, name, cost, quantity): #инизиализация экземпляров класса
+        self.type_of_food = type_of_food.lower().title()    #объявление собстевнной перемнной, которая является копией type_of_food
+        self.name = name.lower().title()    #объявление собстевнной перемнной, которая является копией name
+        self.cost = cost    #объявление собстевнной перемнной, которая является копией cost
+        self.quantity = quantity    #объявление собстевнной перемнной, которая является копией quantity
 
-    def show_info(self):
+    def show_info(self):    #метод для показывания информации
         pass
 
-    def get_full_name(self):
+    def get_full_name(self):    #метод для вывода информации о блюде
         return ''.join([self.name, ', Тип блюда: ', self.type_of_food, ', Цена блюда: ' , str(self.cost), '₽'])
 
-    def get_name(self, number=None):
+    def get_name(self, number=None):    #метода для вывода названия блюд с цифрами, для метода поиска по названию(формирование перечная названий блюд
         if number is not None:
             return ''.join([self.name, ' (', str(number), ')'])
         return ''.join([self.name])
 
-class OrderMain:
+class OrderMain:        #класc OrderMain
     def __init__ (self, table_number, sum_cost):
-        self.table_number = table_number
-        self.sum_cost = sum_cost
+        self.table_number = table_number    #объявление собстевнной перемнной, которая является копией table_number
+        self.sum_cost = sum_cost    #объявление собстевнной перемнной, которая является копией sum_cost
